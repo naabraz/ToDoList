89572496c6c9f63f223c51891653335301a4796c
Natalia Braz <naabraz@gmail.com>
2023-12-20 09:55:11 -0300

    chore: add floating button

--- app/src/main/java/br/com/nataliabraz/todolist/ui/activity/ToDoListActivity.kt
+++ app/src/main/java/br/com/nataliabraz/todolist/ui/activity/ToDoListActivity.kt
@@ -1,7 +1,18 @@
 package br.com.nataliabraz.todolist.ui.activity
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.com.nataliabraz.todolist.databinding.ActivityTodoListBinding
 
 class ToDoListActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        ActivityTodoListBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+    }
 }
