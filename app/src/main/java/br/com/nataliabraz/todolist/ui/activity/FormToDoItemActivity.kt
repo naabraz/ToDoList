309319e1361143c7f431dfd47afb08f07524befa
Natalia Braz <naabraz@gmail.com>
2023-12-22 09:11:31 -0300

    feat: add items to list

--- app/src/main/java/br/com/nataliabraz/todolist/ui/activity/FormToDoItemActivity.kt
+++ app/src/main/java/br/com/nataliabraz/todolist/ui/activity/FormToDoItemActivity.kt
@@ -2,7 +2,9 @@ package br.com.nataliabraz.todolist.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.com.nataliabraz.todolist.dao.ToDoItemDAO
 import br.com.nataliabraz.todolist.databinding.ActivityFormTodoItemBinding
+import br.com.nataliabraz.todolist.model.ToDoItem
 
 class FormToDoItemActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -12,6 +14,25 @@ class FormToDoItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setUpSaveButton()
         setContentView(binding.root)
+    }
+
+    private fun createItem(): ToDoItem {
+        val inputTitle = binding.activityFormTodoItemTitle
+        val title = inputTitle.text.toString()
+
+        return ToDoItem(title = title)
+    }
+
+    private fun setUpSaveButton() {
+        val saveButton = binding.activityFormTodoSave
+        val dao = ToDoItemDAO()
+
+        saveButton.setOnClickListener {
+            val newItem = createItem()
+            dao.add(newItem)
+            finish()
+        }
     }
 }
